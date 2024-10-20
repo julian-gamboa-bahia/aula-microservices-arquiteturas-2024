95211a7faab6a12d33dd9f6ad143630482f603dc
julian-gamboa-bahia <fulanodetal@exemplo.br>
2024-10-20 03:24:08 -0300

    network docker

---
+++ simples-rabittmq/otimizar/Program.cs
@@ -0,0 +1,30 @@
+using Otimizar.Business;
+using Otimizar.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://*:5225");
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Adiciona os controladores
+builder.Services.AddControllers(); // Esta linha é necessária para registrar os controladores
+
+
+builder.Services.AddScoped<RegrasDeNegocio>();
+builder.Services.AddScoped<AlocacaoCaixaService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
